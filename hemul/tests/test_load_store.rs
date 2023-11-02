d2f335fd1c2816991c2100e1066a41e4bf4fbdf3
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2023-11-02 21:53:35 +0100

    More tests

--- hemul/tests/test_load_store.rs
+++ hemul/tests/test_load_store.rs
@@ -34,7 +34,7 @@ fn as_hex(n: u8) -> String {
 
 proptest! {
     #[test]
-    fn test_basic_ld_immediate(reg in registers(), (val, z, n) in zn_tests()) {
+    fn test_load_immediate(reg in registers(), (val, z, n) in zn_tests()) {
         let snapshot = asm_test!(
             format!(
                 r#"
@@ -52,7 +52,7 @@ proptest! {
     }
 
     #[test]
-    fn test_basic_ld_zero_page(reg in registers(), (val, z, n) in zn_tests()) {
+    fn test_load_zero_page(reg in registers(), (val, z, n) in zn_tests()) {
         let snapshot = asm_test!(
             format!(
                 r#"
@@ -72,7 +72,7 @@ proptest! {
     }
 
     #[test]
-    fn test_basic_ld_zero_page_x(reg in registers(), (val, z, n) in zn_tests()) {
+    fn test_load_zero_page_x(reg in registers(), (val, z, n) in zn_tests()) {
         prop_assume!(reg != "X");
 
         let snapshot = asm_test!(
@@ -95,7 +95,7 @@ proptest! {
     }
 
     #[test]
-    fn test_basic_ld_zero_page_y((val, z, n) in zn_tests()) {
+    fn test_load_zero_page_y((val, z, n) in zn_tests()) {
         let snapshot = asm_test!(
             format!(
                 r#"
@@ -115,7 +115,7 @@ proptest! {
     }
 
     #[test]
-    fn test_basic_ld_absolute(reg in registers(), (val, z, n) in zn_tests()) {
+    fn test_load_absolute(reg in registers(), (val, z, n) in zn_tests()) {
         let snapshot = asm_test!(
             format!(
                 r#"
@@ -135,7 +135,7 @@ proptest! {
     }
 
     #[test]
-    fn test_basic_ld_absolute_x(reg in registers(), (val, z, n) in zn_tests()) {
+    fn test_load_absolute_x(reg in registers(), (val, z, n) in zn_tests()) {
         prop_assume!(reg != "X");
 
         let snapshot = asm_test!(
@@ -158,7 +158,7 @@ proptest! {
     }
 
     #[test]
-    fn test_basic_ld_absolute_y(reg in registers(), (val, z, n) in zn_tests()) {
+    fn test_load_absolute_y(reg in registers(), (val, z, n) in zn_tests()) {
         prop_assume!(reg != "Y");
 
         let snapshot = asm_test!(
@@ -181,7 +181,7 @@ proptest! {
     }
 
     #[test]
-    fn test_basic_ld_indexed_indirect((val, z, n) in zn_tests()) {
+    fn test_load_indexed_indirect((val, z, n) in zn_tests()) {
         let snapshot = asm_test!(
             format!(
                 r#"
@@ -202,7 +202,7 @@ proptest! {
     }
 
     #[test]
-    fn test_basic_ld_indirect_indexed((val, z, n) in zn_tests()) {
+    fn test_load_indirect_indexed((val, z, n) in zn_tests()) {
         let snapshot = asm_test!(
             format!(
                 r#"
@@ -221,5 +221,130 @@ proptest! {
         assert_eq!(snapshot.A, val);
         assert_eq!(snapshot.Z, z);
         assert_eq!(snapshot.N, n);
+    }
+
+    #[test]
+    fn test_store_zero_page(reg in registers()) {
+        let snapshot = asm_test!(
+            format!(
+                r#"
+                ;;
+    LD{}    #$66
+    ST{}    $88
+    NOP
+                "#,
+                reg,
+                reg,
+            )
+        );
+        assert_eq!(snapshot.dump[0x0088], 0x66);
+    }
+
+    #[test]
+    fn test_store_zero_page_x(reg in registers()) {
+        prop_assume!(reg != "X");
+
+        let snapshot = asm_test!(
+            format!(
+                r#"
+                ;;
+    LDX     #$05
+    LD{}    #$66
+    ST{}    $88,X
+    NOP
+                "#,
+                reg,
+                reg,
+            )
+        );
+        assert_eq!(snapshot.dump[0x008D], 0x66);
+    }
+
+    #[test]
+    fn test_store_zero_page_y(reg in registers()) {
+        prop_assume!(reg == "X");
+
+        let snapshot = asm_test!(
+            format!(
+                r#"
+                ;;
+    LDY     #$0A
+    LDX     #$66
+    STX     $88,Y
+    NOP
+                "#,
+            )
+        );
+        assert_eq!(snapshot.dump[0x0092], 0x66);
+    }
+
+    #[test]
+    fn test_store_absolute(reg in registers()) {
+        let snapshot = asm_test!(
+            format!(
+                r#"
+                ;;
+    LD{}    #$66
+    ST{}    $1234
+    NOP
+                "#,
+                reg,
+                reg,
+            )
+        );
+        assert_eq!(snapshot.dump[0x1234], 0x66);
+    }
+
+    #[test]
+    fn test_store_absolute_xy(reg in registers()) {
+        prop_assume!(reg != "A");
+        let snapshot = asm_test!(
+            format!(
+                r#"
+                ;;
+    LD{}    #$20
+    LDA     #$66
+    STA     $2000,{}
+    NOP
+                "#,
+                reg,
+                reg,
+            )
+        );
+        assert_eq!(snapshot.dump[0x2020], 0x66);
+    }
+
+    #[test]
+    fn test_store_indexed_indirect(reg in registers()) {
+        prop_assume!(reg == "A");
+        let snapshot = asm_test!(
+                r#"
+                ;;
+    LDX     #$20
+    LDA     #$66
+    STA     ($20,X)
+    NOP
+    .org    $0040
+    .word   $1234
+                "#
+        );
+        assert_eq!(snapshot.dump[0x1234], 0x66);
+    }
+
+    #[test]
+    fn test_store_indirect_indexed(reg in registers()) {
+        prop_assume!(reg == "A");
+        let snapshot = asm_test!(
+                r#"
+                ;;
+    LDY     #$20
+    LDA     #$66
+    STA     ($20),Y
+    NOP
+    .org    $0020
+    .word   $1234
+                "#
+        );
+        assert_eq!(snapshot.dump[0x1254], 0x66);
     }
 }
